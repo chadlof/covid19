5199c011365e09a2552431166ccafd40f28b0d9b
chadlof <chad@chads-MacBook-Pro.local>
2020-06-08 10:00:30 -0500

    Clean up dashboard use useQuesry

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Dashboard } from './pages/Dashboard'
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
+
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://covid19-graphql.now.sh',
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 const  App = () => {
   return (
+    <ApolloProvider client={client}>
     <Dashboard/>
+    </ApolloProvider>
   );
 }
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,46 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/macro'
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { RestLink } from 'apollo-link-rest';
-import { HttpLink } from "apollo-link-http";
-
-
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 import {RowOfCounters} from "../molecules/RowOfCounters"
 import {Title} from "../atoms/Title"
-import gql from 'graphql-tag';
-
-
-// // setup your `RestLink` with your endpoint
-// const restLink = new RestLink({ uri: "https://covidtracking.com/api/v1/states/current.json",
-//                                 headers: {
-//                                   'Content-Type': 'application/json',
-//                                   'Access-Control-Allow-Origin': '*',
-//                                 },
-//                                 method: 'GET'
-//                               });
-
-// // setup your client
-// const client = new ApolloClient({
-//   link: restLink,
-//   cache: new InMemoryCache(),
-// });
-
-// const Query = gql`
-//   query states {
-//     state @rest {
-//       state
-//       positive
-//     }
-//   }
-// `
-
-// client.query({ Query }).then(response => {
-//   console.log(response.data.name);
-// }).catch(error => {
-//   this.error = error;
-//   alert("E " + error);
-// });;
 
 
 
@@ -54,55 +17,49 @@ padding: 16px 0 4px 16px;
 font-size: 22px;
 font-weight:bold;
 `
-
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'https://covid19-graphql.now.sh',
-});
-
-
-const usClient = new ApolloClient({
-  cache,
-  link
-})
-
-usClient
-  .query({
-    query: gql`
-    {
-    # time series data
-  results (countries: ["US", "Canada"], date: { lt: "4/21/2020" }) {
-    country {
-      name
-    }
-    date
-    confirmed
-    deaths
-    recovered
-    growthRate
-  }
-
-  # by country
-  country(name: "US") {
+const GET_US_DATA  = gql`
+{
+  # time series data
+results (countries: ["US", "Canada"]) {
+  country {
     name
-    mostRecent {
-      date(format: "yyyy-MM-dd")
-      confirmed
-    }
+  }
+  date
+  confirmed
+  deaths
+  recovered
+  growthRate
+}
+
+# by country
+country(name: "US") {
+  name
+  mostRecent {
+    date(format: "yyyy-MM-dd")
+    confirmed
   }
 }
-    `
-  })
-  .then(result => console.log(result));
+}`
 
 export const  Dashboard = () => {
+  const { loading, error, data } = useQuery(GET_US_DATA);
+  const isAvaliable = data && data.country && data.results
+  if (isAvaliable) {
+    const mostRecent = data.country.mostRecent
+    const resultsArray = data.results
+    const todaysResults = resultsArray.filter(result => result.date === mostRecent.date)
+    
+    console.log({mostRecent})
+    console.log({resultsArray})
+    console.log({todaysResults})
+}
+
+
+
   return (
       <Wrapper>
         <Title/>
-        <Heading>Cases in the world</Heading>
-        <RowOfCounters/>
         <Heading>Cases in the United States</Heading>
-
         <RowOfCounters/>
 
       </Wrapper>
